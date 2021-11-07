d0c5cf39b5f89c50cb2c2dfc1b58347ef589688b
Maria T. Andersen <mteilmann93@gmail.com>
2021-11-07 13:52:36 +0100

    add min function which takes two integer as arguments. The function will then return the smallest of these two integers

--- Basics/integer.c
+++ Basics/integer.c
@@ -1,6 +1,14 @@
 
 #include <stdio.h>
 
+int min(int a, int b)
+{
+  if (a > b)
+    return b;
+  else
+    return a;
+}
+
 int multiply(int a, int b, int c)
 {
   return a * b * c;
@@ -13,10 +21,11 @@ int minus(int a, int b, int c)
 
 int main()
 {
-  printf("%d \n", multiply(2, 3, 2));
-  printf("%d \n", minus(2, 3, 2));
+  // printf("%d \n", multiply(2, 3, 2));
+  // printf("%d \n", minus(2, 3, 2));
+  printf("%d \n", min(3, 2));
   int sum = 0;
   sum = 2 + 3 + 2;
-  printf("%d \n", sum);
+  // printf("%d \n", sum);
   return sum;
 }
